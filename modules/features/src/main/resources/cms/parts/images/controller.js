2b3d35e20d1837c3a74f1dcb756e3f65c053aaa3
Alex Rodriguez <aro@enonic.com>
2015-05-12 15:00:16 +0200

    Added images part for testing image URL, filters and focal point. Adapted from 6.0 branch changes.

---
+++ modules/features/src/main/resources/cms/parts/images/controller.js
@@ -0,0 +1,115 @@
+var thymeleaf = require('/cms/lib/view/thymeleaf');
+
+var scaleOptions = [
+    {name: 'Scale Max', value: 'scalemax(600)'},
+    {name: 'Scale Wide', value: 'scalewide(600,200)'},
+    {name: 'Scale Block V', value: 'scaleblock(600,200)'},
+    {name: 'Scale Block H', value: 'scaleblock(200,600)'},
+    {name: 'Scale Square', value: 'scalesquare(600)'},
+    {name: 'Scale Width', value: 'scalewidth(600)'},
+    {name: 'Scale Height', value: 'scaleheight(600)'}
+];
+
+var filterOptions = [
+    {name: '-- No Filter --', value: ''},
+    {name: 'Block', value: 'block(10)'},
+    {name: 'Blur', value: 'blur(10)'},
+    {name: 'Border', value: 'border(2,255)'},
+    {name: 'Emboss', value: 'emboss()'},
+    {name: 'Gray Scale', value: 'grayscale()'},
+    {name: 'Invert', value: 'invert()'},
+    {name: 'Rounded', value: 'rounded(10,0,0)'},
+    {name: 'Sharpen', value: 'sharpen()'},
+    {name: 'RGB Adjust', value: 'rgbadjust(100,0,100)'},
+    {name: 'HSB Adjust', value: 'hsbadjust(0,0,0)'},
+    {name: 'Edge', value: 'edge()'},
+    {name: 'Bump', value: 'bump()'},
+    {name: 'Sepia', value: 'sepia(20)'},
+    {name: 'Rotate 90', value: 'rotate90()'},
+    {name: 'Rotate 180', value: 'rotate180()'},
+    {name: 'Rotate 270', value: 'rotate270()'},
+    {name: 'Flip horizontal', value: 'fliph()'},
+    {name: 'Flip vertical', value: 'flipv()'},
+    {name: 'Colorize', value: 'colorize(1,1,1)'},
+    {name: 'Colorize HSB', value: 'hsbcolorize(0xFFFFFFFF)'}
+];
+
+exports.get = function (req) {
+    var imageIds = getImageIds();
+    var imageUrls = [];
+    for (var i = 0; i < imageIds.length; i++) {
+        imageUrls.push(defaultImageUrl(imageIds[i]));
+    }
+
+    var postUrl = execute('portal.componentUrl', {});
+    var params = {
+        imageUrls: imageUrls,
+        postUrl: postUrl,
+        scaleOptions: scaleOptions,
+        filterOptions: filterOptions
+    };
+
+    var view = resolve('images.html');
+    var body = thymeleaf.render(view, params);
+
+    return {
+        contentType: 'text/html',
+        body: body,
+        pageContributions: {
+            bodyEnd: [
+                '<script src="' + execute('portal.assetUrl', {path: 'js/jquery-2.1.4.min.js'}) + '" type="text/javascript"></script>',
+                '<script src="' + execute('portal.assetUrl', {path: 'js/images-part.js'}) + '" type="text/javascript"></script>'
+            ]
+        }
+    };
+
+};
+
+exports.post = function (req) {
+    var filter = req.formParams.filter;
+    var scale = req.formParams.scale;
+
+    var imageIds = getImageIds();
+    var imageUrls = [];
+    for (var i = 0; i < imageIds.length; i++) {
+        var imageUrl = execute('portal.imageUrl', {
+            id: imageIds[i],
+            filter: scale + (filter ? ";" + filter : "")
+        });
+        imageUrls.push(imageUrl);
+    }
+
+    return {
+        contentType: 'application/json',
+        body: {
+            images: imageUrls
+        }
+    };
+};
+
+function getImageIds() {
+    var component = execute('portal.getComponent');
+
+    var imageFolderId = component.config.imageFolder;
+    var imageIds = [];
+    if (imageFolderId) {
+        var result = execute('content.getChildren', {
+            key: imageFolderId,
+            count: 20
+        });
+        for (var i = 0; i < result.contents.length; i++) {
+            var child = result.contents[i];
+            if (child.type === "media:image") {
+                imageIds.push(child._id);
+            }
+        }
+    }
+    return imageIds;
+}
+
+function defaultImageUrl(contentId) {
+    return execute('portal.imageUrl', {
+        id: contentId,
+        filter: 'scalewide(600,400)'
+    });
+}
